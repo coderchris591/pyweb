8a1943646cb2e1d9de5d87c7994078dde30fd8a0
Chris Martinez <chmartinez2014@gmail.com>
2024-09-12 12:47:49 -0500

    restyled home page and guess page for mobile

--- static/myapp.js
+++ static/myapp.js
@@ -1,3 +1,20 @@
 
 
+// Open side navigation
+function toggleMenu() {
+    var nav = document.querySelector('.sideNav');
+    nav.classList.toggle('active');
+}
 
+
+// Close the side navigation if clicked outside
+document.addEventListener('click', function(event) {
+    var nav = document.querySelector('.sideNav');
+    var hamburger = document.querySelector('.hamburger');
+    var isClickInsideNav = nav.contains(event.target);
+    var isClickInsideHamburger = hamburger.contains(event.target);
+
+    if (!isClickInsideNav && !isClickInsideHamburger) {
+        nav.classList.remove('active');
+    }
+});
